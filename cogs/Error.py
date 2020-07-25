f8358c7d915982deca57f17f0bec2fa9c7210f72
Myrvek <66482623+Myrvek@users.noreply.github.com>
2020-07-25 18:00:35 +0300

    Update Error.py

--- cogs/Error.py
+++ cogs/Error.py
@@ -27,6 +27,26 @@ class error(commands.Cog):
 				
 
 			return await ctx.send(embed=embed)
+		import json
+import requests
+
+    @commands.command(aliases=['коронавирус', 'ковид'])
+    async def covid(self, ctx, country):
+        for item in json.loads(requests.get("https://corona.lmao.ninja/v2/countries").text):
+            if item['country'] == country: 
+                embed = discord.Embed(title=f'Статистика Коронавируса | {country}')
+                embed.add_field(name='Выздоровело:',          value=f'{item["recovered"]} человек')
+                embed.add_field(name='Заболеваний:',          value=f'{item["cases"]} человек')
+                embed.add_field(name='Погибло:',              value=f'{item["deaths"]} человек')
+                embed.add_field(name='Заболеваний за сутки:', value=f'+{item["todayCases"]} человек')
+                embed.add_field(name='Погибло за сутки:',     value=f'+{item["todayDeaths"]} человек')
+                embed.add_field(name='Проведено тестов:',     value=f'{item["tests"]} человек')
+                embed.add_field(name='Активные зараженные:',  value=f'{item["active"]} человек')
+                embed.add_field(name='В тяжелом состоянии:',  value=f'{item["critical"]} человек')
+                embed.set_thumbnail(url=item["countryInfo"]['flag'])
+                embed.set_footer(text="© Copyright 2020 хто я#0000 | Все права съедены")
+
+                return await ctx.send(embed=embed)
 def setup(Bot):
 	Bot.add_cog(error(Bot))
 	print("[Cog] Errors загружжен!(32 строки)")
